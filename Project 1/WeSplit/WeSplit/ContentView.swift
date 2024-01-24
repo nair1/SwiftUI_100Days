ee74591b6628058bc43838aa8fef2a378183810c
Sachin Nair <sachinn1118@gmail.com>
2024-01-23 23:12:17 -0600

    added day 16 project code

---
+++ Project 1/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Sachin Nair on 1/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    
+    var body: some View {
+        
+        NavigationStack {
+            Form {
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
