dc3db4676c85492fa67510ff3324705c6fbcaeb1
Sachin Nair <sachinn1118@gmail.com>
2024-02-02 18:34:22 -0600

    added day 23 project code

---
+++ Project 3/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Sachin Nair on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
